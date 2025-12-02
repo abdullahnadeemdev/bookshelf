f81433bd29cff3b85887b8567092bb570356130b
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-02 19:10:04 +0500

    added card to hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import Button from "../shared/button/Button";
+import { SearchIcon } from "../../assets/icons";
 
 const Hero = () => {
   return (
     <div className="text-center">
-      <p className="py-6 text-[83px] text-grayBg font-semibold text-wrap">
+      <p className="py-6 text-[83px] text-grayBg font-semibold text-wrap ">
         READING MAKES THE WORLD HUGE
       </p>
 
-      <div>
-        <div className="bg-grayBg rounded-xl text-start p-4 w-fit max-w-md">
-          <p className="text-wrap text-3xl pr-40 mb-10">
+      <div className="relative">
+        <div className="bg-grayBg rounded-xl text-start p-4 w-fit max-w-xl">
+          <p className="text-wrap text-3xl pr-70 mb-6 z-2">
             FIND SOMETHING TO READ
           </p>
-          <p className="text-wrap">
+          <SearchIcon className="absolute -top-10 h-100! w-100! z-0 left-55 rotate-90 stroke-black!" />
+          <p className="text-wrap mb-18 pr-40 z-2">
             Fancy something unusual and unpredictable? Funny or exciting? No
             problem. Check out the collections we have prepared for you.
           </p>
-          <Button variant="outline">BROWSE NOW</Button>
+          <Button variant="outline" className="z-2">
+            BROWSE NOW
+          </Button>
         </div>
         <div></div>
       </div>
