4766ad2b3571a7aedd97fea9e0e1210f49f93cd5
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-02 15:37:05 +0500

    installed libraries

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import React from "react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), React()],
 });
