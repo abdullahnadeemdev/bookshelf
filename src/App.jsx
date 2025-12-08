f5c8b2dca5d7983faba20718b559b28202afd3c1
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-08 16:32:41 +0500

    homepage complete waiting approval

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Index";
+import Products from "./pages/products/index";
 
 function App() {
   return (
     <Layout>
       <div className="">
-        <Home />
+        {/* <Home /> */}
+        <Products />
       </div>
     </Layout>
   );
---
+++ src/pages/products/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/products/banner/Banner";
+
+const index = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default index;
