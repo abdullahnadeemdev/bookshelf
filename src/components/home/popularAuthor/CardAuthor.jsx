609270cce44b9a4324da0ada8aa5718fea630e4d
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-08 14:10:51 +0500

    added grid to popular author

--- src/components/home/popularAuthor/CardAuthor.jsx
+++ src/components/home/popularAuthor/CardAuthor.jsx
@@ -8,6 +8,96 @@ const CardAuthor = (props) => {
         <div
           className="
             flex
+            w-full h-[20vh]
+            text-black
+            bg-yellow
+            rounded-2xl
+            items-center justify-center
+            max-w-100
+          "
+        >
+          <h1 className="font-bold md:text-2xl">SEE ALL</h1>
+        </div>
+      ) : (
+        <div
+          className="
+            flex flex-col
+            xs:flex-row
+            h-[20vh]
+            max-w-[400px]
+            text-black
+            bg-white
+            rounded-2xl
+          "
+        >
+          <div
+            className="
+            h-10
+            xs:h-full
+              lg:min-w-10 lg:max-w-40 lg:max-h-49
+              flex-4
+              sm:flex-3
+            "
+          >
+            <img
+              src={props.image}
+              alt=""
+              className="
+                object-cover
+                h-full w-full
+                rounded-l-2xl
+                grayscale-100
+              "
+            />
+          </div>
+          <div
+            className="
+              w-full
+              px-2
+              flex-5
+            "
+          >
+            <div
+              className="
+                flex
+                mt-2
+                justify-between items-center
+                mb-3
+                xs:mb-8
+              "
+            >
+              <p
+                className="
+                  w-20
+                "
+              >
+                {props.book}
+              </p>
+              {props.book ? <Bookmark /> : ""}
+            </div>
+            <p
+              className="
+                text-wrap
+                sm:text-base
+                lg:max-w-30 lg:text-xl
+              "
+            >
+              {props.author}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CardAuthor;
+
+/*   <>
+      {props.author === "SEE ALL" ? (
+        <div
+          className="
+            flex
             w-full h-[25vh]
             text-black
             bg-yellow
@@ -83,8 +173,4 @@ const CardAuthor = (props) => {
           </div>
         </div>
       )}
-    </>
-  );
-};
-
-export default CardAuthor;
+    </> */
--- src/components/home/popularAuthor/PopularAuthor.jsx
+++ src/components/home/popularAuthor/PopularAuthor.jsx
@@ -4,7 +4,7 @@ const PopularAuthor = () => {
   return (
     <div className="h-fit mb-10 2xl:h-screen max-w-[1440px] px-4 mx-auto">
       <h1 className="font-semibold text-4xl py-10 pl-6">POPULAR AUTHORS</h1>
-      <div className="flex justify-center pb-5 md:pb-10 w-full gap-3 flex-wrap">
+      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center p-5 md:pb-10 w-full gap-3 ">
         {authorsArray.map((item, index) => (
           <CardAuthor
             key={index}
@@ -19,3 +19,13 @@ const PopularAuthor = () => {
 };
 
 export default PopularAuthor;
+
+// <div className="flex justify-center pb-5 md:pb-10 w-full gap-3 flex-wrap">
+//   {authorsArray.map((item, index) => (
+//     <CardAuthor
+//       key={index}
+//       image={item.image}
+//       author={item.author}
+//       book={item.book}
+//     />
+//   ))}
