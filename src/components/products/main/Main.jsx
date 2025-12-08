da60ca51b9425bd53b20ff02526acf2fb8c875e0
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-08 16:42:23 +0500

    adding main section

--- src/components/products/main/Main.jsx
+++ src/components/products/main/Main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <div></div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Banner from "../../components/products/banner/Banner";
+import Main from "../../components/products/main/Main";
 
 const index = () => {
   return (
     <div>
       <Banner />
+
+      <div className="bg-grayBg mb-5">
+        <Main />
+      </div>
     </div>
   );
 };
