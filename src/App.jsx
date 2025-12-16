494dd52b4c80e0833465610cb99f1b69bf58a7f6
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-16 17:59:01 +0500

    added bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import Layout from "./layout/Layout";
-import Home from "./pages/home/Index";
-import Products from "./pages/products/index";
-import ProductDesc from "./pages/productDesc/Index";
-import Checkout from "./pages/checkout/Index";
 import Router from "./routes";
-import Index from "./pages/bookmarks/Index";
 
 function App() {
   return (
     <Layout>
       <div className="">
         <Router />
-        {/* <Index /> */}
       </div>
     </Layout>
   );
