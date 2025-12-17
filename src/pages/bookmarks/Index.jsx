1ba6467935dd9b72a1bdd5228ff6dd819b55e8b3
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-17 19:06:21 +0500

    error handling in bookmark

--- src/pages/bookmarks/Index.jsx
+++ src/pages/bookmarks/Index.jsx
@@ -2,25 +2,31 @@ import CardBookmark from "./CardBookmark";
 import { ArrayBestSellers as array } from "../../utils/utils";
 
 const Index = () => {
-  let booksArray = JSON.parse(localStorage.getItem("bookmarks")) || "";
+  let booksArray = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
   return (
     <div className="h-fit pb-10 mb-10 2xl:h-screen max-w-[1440px] px-4 bg-blackC">
       <h1 className="font-semibold text-4xl py-10">Favourite Books</h1>
       <div className="flex justify-evenly gap-3 flex-wrap">
-        {booksArray?.map((item, index) => (
-          <CardBookmark
-            key={index}
-            image={item.image}
-            author={item.author}
-            title={item.title}
-            comts={item.comts}
-            star={item.star}
-            people={item.people}
-            price={item.price}
-            saleP={item.saleP}
-          />
-        ))}
+        {booksArray.length > 0 ? (
+          booksArray?.map((item, index) => (
+            <CardBookmark
+              key={index}
+              image={item.image}
+              author={item.author}
+              title={item.title}
+              comts={item.comts}
+              star={item.star}
+              people={item.people}
+              price={item.price}
+              saleP={item.saleP}
+            />
+          ))
+        ) : (
+          <p className="text-red text-4xl font-semibold">
+            There are no books added
+          </p>
+        )}
       </div>
     </div>
   );
