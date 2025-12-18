a5bd0f6466e6c7e7fda485d95d6cfcf1cdc49ccf
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-18 18:42:47 +0500

    started auth

--- src/components/checkout/checkout1/Index.jsx
+++ src/components/checkout/checkout1/Index.jsx
@@ -48,8 +48,12 @@ const Index = () => {
         <div className="flex justify-between">
           <div>
             <ContactInfo var={orderState} fun={setOrderState} />
-            <Shipping var={orderState} fun={setOrderState} />
-            <Payment var={orderState} fun={setOrderState} />
+            {!orderState.contactBtn && (
+              <>
+                <Shipping var={orderState} fun={setOrderState} />
+                <Payment var={orderState} fun={setOrderState} />
+              </>
+            )}
           </div>
 
           <OrderSummay />
@@ -60,104 +64,3 @@ const Index = () => {
 };
 
 export default Index;
-
-{
-  /* <div>
-  {orderState.contactBtn ? (
-    <ContactInfo var={orderState} fun={setOrderState} />
-  ) : (
-    ""
-  )}
-  <CloseCard
-    var={orderState}
-    hint="editContactBtn"
-    hint2="contactBtn"
-    fun={setOrderState}
-    head="CONTACT INFORMATION"
-    title="Name"
-    info="Ali Ahmed"
-    title2="Contact"
-    info2="+345098656"
-  />
-  <p className="h-2"></p>
-  <Shipping var={orderState} fun={setOrderState} />
-</div> */
-}
-{
-  /* <div className="">
-  {(() => {
-    if (
-      orderState.shipBtn === false &&
-      orderState.contactBtn === true &&
-      orderState.paymentBtn === false
-    ) {
-      return <ContactInfo var={orderState} fun={setOrderState} />;
-    } else if (
-      orderState.editContactBtn === true &&
-      orderState.shipBtn === false &&
-      orderState.contactBtn === false &&
-      orderState.paymentBtn === true
-    ) {
-      return <ContactInfo var={orderState} fun={setOrderState} />;
-    } else if (
-      (orderState.shipBtn === true &&
-        orderState.contactBtn === false &&
-        orderState.paymentBtn === false) ||
-      orderState.editShipBtn === true
-    ) {
-      return (
-        <>
-          <CloseCard
-            var={orderState}
-            hint="editContactBtn"
-            hint2="contactBtn"
-            fun={setOrderState}
-            head="CONTACT INFORMATION"
-            title="Name"
-            info="Ali Ahmed"
-            title2="Contact"
-            info2="+345098656"
-          />
-          <p className="h-2"></p>
-          <Shipping var={orderState} fun={setOrderState} />
-        </>
-      );
-    } else if (
-      orderState.shipBtn === false &&
-      orderState.contactBtn === false &&
-      orderState.paymentBtn === true
-    ) {
-      return (
-        <>
-          <CloseCard
-            var={orderState}
-            hint="editContactBtn"
-            fun={setOrderState}
-            head="CONTACT INFORMATION"
-            title="Name"
-            info="Ali Ahmed"
-            title2="Contact"
-            info2="+345098656"
-          />
-          <p className="h-2"></p>
-          <CloseCard
-            hint="shipBtn"
-            hint2="editShipBtn"
-            var={orderState}
-            fun={setOrderState}
-            head="SHIPPING METHOD"
-            title="Date"
-            info="2 May,25"
-            title2="Address"
-            info2="Johar town R2 block"
-          />
-          <p className="h-2"></p>
-          <Payment var={orderState} fun={setOrderState} />
-        </>
-      );
-    } else {
-      return <p>Error loading pages</p>;
-    }
-  })()}
-</div> */
-}
