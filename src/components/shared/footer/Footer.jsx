c46fcfa48092d622fc9e92db245919b7374c93b4
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2025-12-29 18:27:15 +0500

    fix(footer):home route on logo

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -10,12 +10,14 @@ const Footer = () => {
   return (
     <div className="max-w-[1440px] mx-auto ">
       <div className="flex items-center justify-between w-full py-5 px-5">
-        <div className="flex items-center gap-2 ">
-          <span className="w-[4vw]">
-            <Logo />
-          </span>
-          <h3 className="font-bold  md:text-xl lg:text-2xl">BIG BOOKSHELF</h3>
-        </div>
+        <Link to="/">
+          <div className="flex items-center gap-2 ">
+            <span className="w-[4vw]">
+              <Logo />
+            </span>
+            <h3 className="font-bold  md:text-xl lg:text-2xl">BIG BOOKSHELF</h3>
+          </div>
+        </Link>
         <div className="flex items-center gap-4">
           <Link to="https://www.instagram.com/">
             <InstagramIcon />
