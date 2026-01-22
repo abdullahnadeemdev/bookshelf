7b1c83aeacd3b22019c3d034eefa4188c15cfe1c
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-01-22 17:51:46 +0500

    Fixing file casing issues for deployment

--- src/routes/GuestRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router";
-
-const GuestRoute = () => {
-  const user = useSelector((state) => state?.auth?.user) || null;
-  return !user ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default GuestRoute;
--- src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router";
-
-const ProtectedRoute = () => {
-  const user = useSelector((state) => state?.auth?.user) || null;
-  return user ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
