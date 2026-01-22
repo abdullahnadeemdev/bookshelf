8a75a909541a9a750ec9b304755bb2f84dea5d03
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-01-22 11:01:13 +0500

    fix(bookmarks):added mx auto to bookmarks

--- src/pages/bookmarks/Index.jsx
+++ src/pages/bookmarks/Index.jsx
@@ -5,7 +5,7 @@ const Index = () => {
   const booksArray = useSelector((state) => state?.book?.items) || [];
 
   return (
-    <div className=" pb-10 my-10 h-screen max-w-[1440px] px-4 bg-blackC">
+    <div className=" pb-10 my-1 h-[85.5vh] mx-auto rounded-[20px] max-w-[1440px] px-4 bg-blackC">
       <h1 className="font-semibold text-4xl py-10">Favourite Books</h1>
       <div className="flex justify-evenly gap-3 flex-wrap">
         {booksArray.length > 0 ? (
